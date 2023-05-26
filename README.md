6fa86889431e1d105d837831933e4c1c9b554ad8
NiGitaMyrGit <201906799@post.au.dk>
2023-05-26 13:32:20 +0200

    Script updated to output tables with Rel_Freq

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Lang_assignment1
-This is the repository for the first assignment in the course Language Analytics from the bachelors elective [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10171067&assignment_repo_type=AssignmentRepo)
-# Extracting linguistic features using spaCy
-
+# Assignment 1 - Extracting linguistic features using spaCy
+This is the repository for the first assignment in the course Language Analytics from the bachelors elective Cultural Data Science
+## 1. Contributions
+The code was written by me independently.
+## 2. Assignment description from instructor
 This assignment concerns using ```spaCy``` to extract linguistic information from a corpus of texts.
 
 The corpus is an interesting one: *The Uppsala Student English Corpus (USE)*. All of the data is included in the folder called ```in``` but you can access more documentation via [this link](https://ota.bodleian.ox.ac.uk/repository/xmlui/handle/20.500.12024/2457).
@@ -20,7 +21,7 @@ For this exercise, you should write some code which does the following:
 |file2.txt|---|---|---|---|---|---|---|
 |etc|---|---|---|---|---|---|---|
 
-## Objective
+### 2.1 Objective
 
 This assignment is designed to test that you can:
 
@@ -28,16 +29,36 @@ This assignment is designed to test that you can:
 2. Use ```spaCy``` to extract linguistic information from text data;
 3. Save those results in a clear way which can be shared or used for future analysis
 
-## Some notes
+### 2.2 Some notes
 
 - The data is arranged in various subfolders related to their content (see the [README](in/README.md) for more info). You'll need to think a little bit about how to do this. You should be able do it using a combination of things we've already looked at, such as ```os.listdir()```, ```os.path.join()```, and for loops.
 - The text files contain some extra information that such as document ID and other metadata that occurs between pointed brackets ```<>```. Make sure to remove these as part of your preprocessing steps!
 - There are 14 subfolders (a1, a2, a3, etc), so when completed the folder ```out``` should have 14 CSV files.
 
-## Additional comments
+### 2.3 Additional comments
 
 Your code should include functions that you have written wherever possible. Try to break your code down into smaller self-contained parts, rather than having it as one long set of instructions.
 
 For this assignment, you are welcome to submit your code either as a Jupyter Notebook, or as ```.py``` script. If you do not know how to write ```.py``` scripts, don't worry - we're working towards that!
 
 Lastly, you are welcome to edit this README file to contain whatever informatio you like. Remember - documentation is important!
+
+## 3. Methods
+The script `extract_features.py` located in the folder [src](https://github.com/NiGitaMyrGit/Lang_assignment1/tree/e4ee062a0a23ffa7ba3717f330c92bbe2f22da8e/src)
+
+## 4. Usage
+The script is written in python 3.10.7. Make sure thsi is the python version you have installed.
+### 4.1 Installing packages
+From the commandline, located in the main directory (use `cd path/to/Lang_assignment1`), run the command `bash setup.sh`. This will install all required packages from the `requirements.txt`file.
+### 4.2 Retrieve data
+**The Uppsala Student English Corpus (USE)** 
+The data is located in the folder [in]() as a zip-file.
+In order to unzip the data, from the commandline, located in the folder [in](), run the command `unzip USEcorpus.zip`
+This will unpack a folder called [USEcorpus] with 14 subfolder names [a1]-[a5], [b1]-[b8] and [c1].
+The zip.file was retrived from [this link](https://ota.bodleian.ox.ac.uk/repository/xmlui/handle/20.500.12024/2457).
+More information can be found in the [readme.md] located in the [in] folder.
+### 4.3 Run the script
+From the command line, located in the main directory, run the command `python3 src/extract_features`
+The script produces a table for each subfolder, showcasing the relative frequency of relative frequency of nouns 
+## 5. Results
+The
---
+++ src/extract_features.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# if not in the 'src' folder cd into it: '/work/cds-lang/assignment-1---linguistic-analysis-using-nlp-NiGitaMyrGit/src'
+# setup
+import spacy
+nlp = spacy.load("en_core_web_sm")
+import os
+import pandas as pd
+import sys
+import fileinput
+import pathlib
+from pathlib import Path
+import csv
+import fnmatch
+
+# Set the main directory 'main_dir' (USEcorpus) where the files are located
+main_dir = os.path.join("in", "USEcorpus")
+
+def run():
+    # Specifying that we are looking at the subdirectories 'dir' (a1-c1) within the main directory
+    for subdir in os.listdir(main_dir):
+        # making a sub path list
+        subdir_path = os.path.join(main_dir, subdir)
+        # creating a results list to be used for appending the results
+        subdir_results = []
+
+        for filename in os.listdir(subdir_path):
+            #  creating empty lists for the parts-of-speech (POS)
+            verb_count = 0
+            noun_count = 0
+            adj_count = 0
+            adv_count = 0
+            per_count = 0
+            loc_count = 0
+            org_count = 0
+            # creating empty sets, since sets inherently only include unique values
+            per_set = set()
+            loc_set = set()
+            org_set = set()
+            # making a list that specifies looking at the files inside the sub directory path
+            file_path = os.path.join(subdir_path, filename)
+
+            #opening the files
+            with open(file_path, 'r', encoding='latin-1') as file:
+                #creating a doc object
+                contents = file.read()
+                # Remove lines containing  "< >" and the doc-id doc.id=
+                remove_pattern = ['<', ">", "doc\.id=[0-9]*\.[abc][0-9]"]
+                lines = contents.split('\n')
+                filtered_lines = []
+                for line in lines:
+                    if not any(fnmatch.fnmatch(line, pattern) for pattern in remove_pattern):
+                        filtered_lines.append(line)
+                contents = ''.join(filtered_lines)
+                
+                #creating a doc object¨
+                doc = nlp(contents)
+
+            #counting the POS
+            for token in doc:
+                if token.pos_ == 'VERB':
+                    verb_count += 1
+                if token.pos_ == 'NOUN':
+                    noun_count += 1
+                if token.pos_ == 'ADJ':
+                    adj_count += 1
+                if token.pos_ == 'ADV':
+                    adv_count += 1
+
+            #counting the unique entities
+            for ent in doc.ents:
+                if ent.label_ == 'PER':
+                    per_set.add(ent.text)
+                    per_count += 1
+                elif ent.label_ == 'LOC':
+                    loc_set.add(ent.text)
+                    loc_count += 1
+                elif ent.label_ == 'ORG':
+                    org_set.add(ent.text)
+                    org_count += 1
+
+            # counting the relative frequency of POS per 10000 words
+            rel_noun_freq = (noun_count / len(doc)) * 10000
+            rel_verb_freq = (verb_count / len(doc)) * 10000
+            rel_adj_freq =  (adj_count / len(doc)) * 10000
+            rel_adv_freq =  (adv_count / len(doc)) * 10000
+            # splitting filename into tuple containing basename of the file.txt without directory path
+            filename_only = os.path.splitext(os.path.basename(file_path))[0]
+            # gathering the output and appending it to the subdir_results list
+            output = [subdir, filename_only, rel_noun_freq, rel_verb_freq, rel_adj_freq, rel_adv_freq, per_count, loc_count, org_count]
+            subdir_results.append(output)
+        #creating the CSV datatables in the out-folder
+        data = pd.DataFrame(subdir_results, columns=['Subdirectory', 'Filename', 'RelFreq NOUN', 'RelFreq VERB', 'RelFreq ADJ', 'RelFreq ADV', 'Unique PER', 'Unique LOC', 'Unique ORG'])
+        csv_path = os.path.join("out", subdir + '_table.csv')
+        data.to_csv(csv_path, index=False)
+
+# calling main function
+if __name__== "__main__":
+    run()
